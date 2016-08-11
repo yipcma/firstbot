bc7e778e8ca1907b48d3fbc15e221d39614a853e
Andrew Yip <yipcma@gmail.com>
2016-08-12 00:42:00 +0300

    add sendTextMessage method

--- index.js
+++ index.js
@@ -43,7 +43,27 @@ app.post('/webhook', function (req, res) {
     res.sendStatus(200)
 })
 
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
 const token = process.env.FB_PAGE_ACCESS_TOKEN
+
 
 // Spin up the server
 app.listen(app.get('port'), function() {
