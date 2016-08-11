ef49572f9f42a86dda72b5176e459967b4617ee7
Andrew Yip <yipcma@gmail.com>
2016-08-12 00:28:31 +0300

    add post method for echoing

--- index.js
+++ index.js
@@ -30,6 +30,21 @@ app.get('/webhook', function(req, res) {
   }
 });
 
+app.post('/webhook', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
+
+const token = process.env.FB_PAGE_ACCESS_TOKEN
+
 // Spin up the server
 app.listen(app.get('port'), function() {
   console.log('running on port', app.get('port'))
